d2d5b976160e80bb0f4d7672bcfbbece69ad6150
LazersDen <dan.zajczev2017@yandex.ru>
2020-01-19 11:47:07 +0300

    complit

--- src/com/company/Wirte.java
+++ src/com/company/Wirte.java
@@ -7,7 +7,7 @@ public class Wirte {
 
     FileWriter fl = new FileWriter("Chet.txt");
 
-    public Wirte(int id,int money,String [][]a) throws IOException {
+    public Wirte(int id,int money,String a[][]) throws IOException {
         for (int i=0;i<a.length;i++){
             for (int t=0;t<a[0].length;t++){
                 if(a[i][0].equals(id+"")){
@@ -16,6 +16,7 @@ public class Wirte {
                 }else {
                     fl.append(a[i][0]+a[i][1]);
                 }
+                fl.flush();
             }
         }
 
@@ -24,3 +25,4 @@ public class Wirte {
 
 
 }
+
